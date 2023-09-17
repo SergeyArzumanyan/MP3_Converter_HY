bae43f1f4e10bed9c33e2f58a4c433859600adb3
Sergey Arzumanyan <Sergey.Arzumanyan.work@gmail.com>
2023-09-17 23:01:02 +0400

    Added Stg/Prod Environment Logic To The Project, + Some Firebase Config Changes.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,23 @@
 export const environment =  {
   production: "false",
   firebase: {
-    apiKey: "AIzaSyDnDbbyQTK12uqXuj5Sy9I9xMuNOPBTMNg",
-    authDomain: "mp3-converter-hy.firebaseapp.com",
-    projectId: "mp3-converter-hy",
-    storageBucket: "mp3-converter-hy.appspot.com",
-    messagingSenderId: "561731177594",
-    appId: "1:561731177594:web:b0cda2a4bd679944e0bfc3"
+    apiKey: "AIzaSyA1ngQACa0r27tF8iVuXvwT8Pr5elfWUFc",
+    authDomain: "mp3-converter-hy-stg.firebaseapp.com",
+    projectId: "mp3-converter-hy-stg",
+    storageBucket: "mp3-converter-hy-stg.appspot.com",
+    messagingSenderId: "701871051292",
+    appId: "1:701871051292:web:6cef9d21f6991a37ae45d9"
   },
 };
+
+// export const environment =  {
+//   production: "true",
+//   firebase: {
+//     apiKey: "AIzaSyDnDbbyQTK12uqXuj5Sy9I9xMuNOPBTMNg",
+//     authDomain: "mp3-converter-hy.firebaseapp.com",
+//     projectId: "mp3-converter-hy",
+//     storageBucket: "mp3-converter-hy.appspot.com",
+//     messagingSenderId: "561731177594",
+//     appId: "1:561731177594:web:b0cda2a4bd679944e0bfc3"
+//   },
+// };
